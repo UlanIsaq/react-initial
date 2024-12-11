ecfc713484d15fa2c2802a01be891c00adb8e8a4
Iskanadzhiev <ulugbek.iskanadzhiev@sogeti.com>
2024-12-10 19:15:09 -0500

    without JSX application

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import './App.css';
 import { createElement } from 'react';
 
 export const App = () => {
-	return (
-		<div className="App">
-			{/*-- imperative */}
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>
-					CurrentDate
-					<div>{new Date().toLocaleDateString()}</div> {/*-- declarative */}
-				</p>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { className: 'App-logo', src: logo, alt: 'logo' }),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				'and save to reload',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn',
+			),
+			createElement(
+				'p',
+				null,
+				'CurrentDate',
+				createElement('div', null, new Date().toLocaleDateString()),
+			),
+		),
 	);
 };
